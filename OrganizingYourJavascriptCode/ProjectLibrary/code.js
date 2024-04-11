3a81c17f0a22f328579a40678c12c6923a0a5a71
BillLaptop <earnappbill@gmail.com>
2024-04-12 02:31:58 +0300

    Finish book display as card

--- OrganizingYourJavascriptCode/ProjectLibrary/code.js
+++ OrganizingYourJavascriptCode/ProjectLibrary/code.js
@@ -27,25 +27,44 @@ function Book(title, author, pages, read) {
     }
 }
 
+function createBook(bookName) {
+    const card = document.createElement('div');
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const year = document.createElement('div');
+
+    card.classList.add('card');
+    container.appendChild(card);
+
+    title.textContent = bookName.title;
+    card.appendChild(title).classList.add('title');
+    
+    card.appendChild(author).classList.add('author');
+    author.textContent = bookName.author;
+
+    card.appendChild(year).classList.add('year');
+    year.textContent = bookName.pages;
+}
+
 function addBookToLibrary(library) {
     
 }
 
 function showLibrary(library) {
     for (let book of library) {
-        const div = document.createElement('div')
+        // const div = document.createElement('div')
         
-        let text = book.title + " " + book.author + ' ' + book.pages;
-        div.textContent = text;
-        container.appendChild(div);
+        // let text = book.title + " " + book.author + ' ' + book.pages;
+        // div.textContent = text;
+        // container.appendChild(div);
         
-
+        createBook(book);
         console.log(book);
 
     }
 }
 
 myLibrary[0] = new Book('Harry Potter', 'Tatsopoulos', 1992, true);
-
+myLibrary[1] = new Book('Hobbit', 'Tolkin', 1000, false);
 
 showLibrary(myLibrary);
